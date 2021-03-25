e1739ed49221a8a0816f236c0bdc74b00abf23a2
juno <selectjuno@gmail.com>
2021-03-25 23:02:37 +0900

    Http Method에 의한 API CRUD 작성

--- src/main/java/com/juno/board/controller/ReplyController.java
+++ src/main/java/com/juno/board/controller/ReplyController.java
@@ -1,6 +1,7 @@
 package com.juno.board.controller;
 
 import com.juno.board.dto.ReplyDTO;
+import com.juno.board.entity.Reply;
 import com.juno.board.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -8,10 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +22,35 @@ public class ReplyController {
     private final ReplyService replyService; // 자동 주입을 위해 final
 
     @GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ReplyDTO>> getListByBoard(
-            @PathVariable("bno") Long bno
-    ) {
+    public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
         log.info("bno : " + bno);
 
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+        log.info(replyDTO);
+
+        return new ResponseEntity<>(replyService.register(replyDTO), HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+        log.info(replyDTO);
+
+        replyService.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        log.info("RNO: " + rno);
+
+        replyService.remove(rno);
+
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
 }
